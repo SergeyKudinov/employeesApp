7fd2e539df8e3a572968ee154cab0e87228b70ae
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2022-09-20 22:05:00 +0300

    start project, make structure

---
+++ src/components/app/app.js
@@ -0,0 +1,22 @@
+import AppInfo from '../app-info/app-info';
+import './app.css';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppInfo/>
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            <EmployeesList/>
+            <EmployeesAddForm/>
+        </div>
+    );
+};
+
+export default App;
